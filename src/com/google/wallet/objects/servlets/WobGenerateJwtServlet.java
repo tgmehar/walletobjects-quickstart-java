4ec81771a7cefd05ce5d4ccea78b942aa93a65fd
peng ying <pying@google.com>
2013-08-08 13:08:55 -0700

    Adding http origin

--- src/com/google/wallet/objects/servlets/WobGenerateJwtServlet.java
+++ src/com/google/wallet/objects/servlets/WobGenerateJwtServlet.java
@@ -54,6 +54,7 @@ public class WobGenerateJwtServlet extends HttpServlet{
     List<String> origins = new ArrayList<String>();
     origins.add("http://localhost:8888");
     origins.add("https://localhost:8888");
+    origins.add("http://wobs-quickstart.appspot.com");
     origins.add("https://wobs-quickstart.appspot.com");
 
     WobPayload payload = new WobPayload();
